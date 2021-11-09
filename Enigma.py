d0c6f676d783371b800d1bc0a7d60feb33403a80
Seifeldin-Abdelkarim <73817357+Seifeldin-Abdelkarim@users.noreply.github.com>
2021-11-09 16:30:41 +0200

    Update Enigma.py

--- Enigma.py
+++ Enigma.py
@@ -6,20 +6,24 @@ countM = 0
 countS = 0
 
 
+def applyPlugboard(character):
+    for j in range(len(plugboard)):
+        if character == plugboard[j][0]:
+            character = plugboard[j][1]
+        elif character == plugboard[j][1]:
+            character = plugboard[j][0]
+    return character
+
+
 def rotate(countF, countM, countS):
     countF += 1
-    insert = rotorslist[r3][0]
     rotorslist[r3].append(rotorslist[r3].pop(0))
     if countF == 62:
         countM += countM
-        insert = rotorslist[r2][0]
-        rotorslist[r2].insert(-1, insert)
-        rotorslist[r2].pop(0)
+        rotorslist[r2].append(rotorslist[r2].pop(0))
         countF = 0
     if countM == 62:
-        insert = rotorslist[r1][0]
-        rotorslist[r1].insert(-1, insert)
-        rotorslist[r1].pop(0)
+        rotorslist[r1].append(rotorslist[r1].pop(0))
         countM = 0
         countS += 1
     if countS == 62:
@@ -73,18 +77,91 @@ def options():
             print("please don't repeat the same Rotor. ")
         else:
             break
-    return r3, r2, r1
+
+    while True:
+        try:
+            choice = str(input("Are you going to use custom ring settings? y or n: "))
+            choice = choice.lower()
+        except ValueError:
+            print("you entered the wrong data type. ")
+            continue
+        if choice != "y" and choice != "n":
+            print("please enter only the letters Y or N for your choice. ")
+        else:
+            break
+    if choice == "y":
+        ringset = input("Enter the ringsettings you want to use: ")
+        while ringset not in Alpha:
+            print("you entered an invalid character.")
+            ringset = input("please try again: ")
+    while True:
+        try:
+            choice = str(input("Are you going to choose a specific offset? y or n: "))
+            choice = choice.lower()
+        except ValueError:
+            print("you entered the wrong data type. ")
+            continue
+        if choice != "y" and choice != "n":
+            print("please enter only the letters Y or N for your choice. ")
+        else:
+            break
+    offset3 = 0
+    offset2 = 0
+    offset1 = 0
+    if choice == "y":
+        offset3 = input("Enter the offset value for rotor 3: ")
+        while offset3 not in Alpha:
+            print("you entered an invalid character.")
+            offset3 = input("please try again: ")
+        offset2 = input("Enter the offset value for rotor 2: ")
+        while offset2 not in Alpha:
+            print("you entered an invalid character.")
+            offset2 = input("please try again: ")
+        offset1 = input("Enter the offset value for rotor 1: ")
+        while offset1 not in Alpha:
+            print("you entered an invalid character.")
+            offset1 = input("please try again: ")
+
+    text = input("please enter the text: ")
+    for char in text:
+        while char not in Alpha:
+            print("you entered an invalid character: ")
+            text = input("please try again: ")  # keeps repeating
+
+    return r3, r2, r1, text, offset1, offset2, offset3
 
 
-def encrypt(text, Alphabet):
+def ringsettings(text):
+    pass
+
+
+def encrypt(text, Alphabet, offset1, offset2, offset3):
     character = ""
     global countF, countM, countS
-    for c in text:
+    # temp = []
+    for offset, rotor in ((offset3, r3), (offset2, r2), (offset1, r1)):
+        if offset != 0:
+            index = Alphabet.index(offset)
+
+            print("index: ", index)
+
+            # temp.extend(rotorslist[rotor][:index])
+            rotorslist[rotor].extend(rotorslist[rotor][:index])
+
+            print("after extending: ", rotorslist[rotor])
+
+            del rotorslist[rotor][:index]
+
+            print("after delete: ", rotorslist[rotor])
+
+    for character in text:
         countF, countM, countS = rotate(countF, countM, countS)
-        character = applyPlugboard(c)
+        character = applyPlugboard(character)
 
         for rotor, count in ((r3, countF), (r2, countM), (r1, countS)):
-            character = rotorslist[rotor][Alphabet.index(character) + count]
+            # if Alphabet.index(character) + count > len(rotorslist[rotor]):
+            #     count -= 62
+            character = rotorslist[rotor][Alphabet.index(character)]
             print(character)
 
         character = refB[Alphabet.index(character)]
@@ -92,25 +169,18 @@ def encrypt(text, Alphabet):
 
         for rotor, count in ((r1, countS), (r2, countM), (r3, countF)):
             charindex = rotorslist[rotor].index(character)
-            character = Alphabet[charindex - count]
+            character = Alphabet[charindex]
             print(character + " 1")
 
         character = applyPlugboard(character)
-
-        # rotate(countF, countM)
         Atext.append(character)
     s = "".join(Atext)
     print(s)
     return Atext
 
 
-def applyPlugboard(character):
-    for j in range(len(plugboard)):
-        if character == plugboard[j][0]:
-            character = plugboard[j][1]
-        elif character == plugboard[j][1]:
-            character = plugboard[j][0]
-    return character
+def mainmenu():
+    pass
 
 
 countF = 0
@@ -118,9 +188,9 @@ countM = 0
 countS = 0
 Atext = []
 
-r3, r2, r1 = options()
+r3, r2, r1, text, offset1, offset2, offset3 = options()
 
 # text = "A"
 
-text = input("here: ")
-encrypt(text, Alpha)
+# text = input("here: ")
+encrypt(text, Alpha, offset1, offset2, offset3)
